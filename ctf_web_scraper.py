440f7b10e015f00de670ad646ce072eb4a7acb06
RedHoodHacker <limbo_shell@protonmail.com>
2024-01-02 23:57:32 -0600

    base page returned...

--- ctf_web_scraper.py
+++ ctf_web_scraper.py
@@ -8,7 +8,9 @@ import regex
 
 base_url = 'https://ctftime.org/event/list/?year=2024&online=-1&format=0&restrictions=-1'
 
-page = requests.get(base_url)
+headers = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+
+page = requests.get(base_url, headers=headers)
 
 print(page.text)
 
