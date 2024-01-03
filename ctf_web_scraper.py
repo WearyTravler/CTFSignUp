94819b66776e65e7039c2ace2405e55b75c1efe3
RedHoodHacker <limbo_shell@protonmail.com>
2024-01-02 23:55:38 -0600

    base variables

--- ctf_web_scraper.py
+++ ctf_web_scraper.py
@@ -4,3 +4,14 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 import regex
+
+
+base_url = 'https://ctftime.org/event/list/?year=2024&online=-1&format=0&restrictions=-1'
+
+page = requests.get(base_url)
+
+print(page.text)
+
+#no headers returns 403 forbidden
+
+soup = BeautifulSoup(page.content, "html.parser")
